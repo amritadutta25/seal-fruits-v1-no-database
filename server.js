39069d12bcb933ef07b3a1ceb6b6dd54a9e85801
Amrita Dutta <amrita.dutta1497@gmail.com>
2023-12-16 12:25:13 -0500

    Added new and create routes

--- server.js
+++ server.js
@@ -13,7 +13,19 @@ app.use(express.static("public")) // use a "public" folder for files
 // public/style.css -> /style.css
 // public/app.js -> /app.js
 
-// fruits index route
+// express.urlencoded (parse url encoded bodies)
+// add the data to req.body
+// used to read the form submitted through the NEW route
+app.use(express.urlencoded({extended: true}))
+
+
+
+
+//******************
+//ROUTES
+//****************** 
+
+// fruits INDEX route
 // get request to /fruits
 // return all fruits
 app.get("/fruits", (req, res) => {
@@ -23,7 +35,45 @@ app.get("/fruits", (req, res) => {
     res.render("index.ejs", {fruits})
 })
 
-// fruits show route
+// NEW Route - render a page with a form
+// GET request to /fruits/new
+// allow us to have a form to create a new fruit
+app.get("/fruits/new", (req, res) => {
+
+    // render a template with our form
+    res.render("new.ejs")  // new.ejs => ./views/ + new.js
+})
+
+
+// CREATE ROUTE - Receives the FORM data and creates the fruit
+// POST request /fruits
+// create a fruit from the form data, then redirect back to index
+app.post("/fruits", (req, res) => {
+    // get the form data from the request
+    const body = req.body   // requests have a header and a body, getting the body of the request here
+
+    //send back the form data as JSON
+    // res.send(body)
+
+    //convert the 'readyToEat' to true or false, because clicking the checkbox sends over readyToEat as 'on' or doesn't send the readyToEat param at all if checkbox is unchecked
+    if (body.readyToEat === 'on') 
+    {
+        body.readyToEat = true
+    }
+    else {
+        body.readyToEat = false
+    }
+
+    // add the fruit to the array
+    fruits.push(body)
+
+    // redirect them back to index page
+    res.redirect("/fruits")
+})
+
+
+
+// fruits SHOW route
 // get request to /fruits/:id
 // return a single fruit
 app.get("/fruits/:id", (req, res) => {
